026b2ab9172a1d27f544c59e57caa890a32fd331
Juan Felipe Barrera <juan.f.barrera01@gmail.com>
2020-01-07 23:00:37 -0500

    inicio

---
+++ src/main/java/com/bolsadeideas/springboot/app/models/dao/ClienteDaoImpl.java
@@ -0,0 +1,30 @@
+package com.bolsadeideas.springboot.app.models.dao;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.bolsadeideas.springboot.app.models.entity.Cliente;
+
+
+
+@Repository
+public class ClienteDaoImpl implements IClienteDao{
+	@PersistenceContext
+	private EntityManager em;
+	
+	
+	
+	@Transactional(readOnly = true)
+	@SuppressWarnings("unchecked")
+	@Override
+	public List<Cliente> findAll() {
+		
+		return em.createQuery("from Cliente").getResultList();
+	}
+
+}
