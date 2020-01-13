9443fa4a5040d72f70c70c97ba36b30b53c34713
Juan Felipe Barrera <juan.f.barrera01@gmail.com>
2020-01-13 17:30:55 -0500

    agregando servicio

--- src/main/java/com/bolsadeideas/springboot/app/controllers/ClienteController.java
+++ src/main/java/com/bolsadeideas/springboot/app/controllers/ClienteController.java
@@ -9,18 +9,22 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
 
 import com.bolsadeideas.springboot.app.models.dao.IClienteDao;
 import com.bolsadeideas.springboot.app.models.entity.Cliente;
+import com.bolsadeideas.springboot.app.models.service.IClienteService;
 
 @Controller
+@SessionAttributes("cliente")
 public class ClienteController {
 	
 	@Autowired
-	
-	private IClienteDao clienteDao;
+	private IClienteService clienteDao;
 	
 	@RequestMapping(value="/listar",method = RequestMethod.GET)
 	public String listar(Model model) {
@@ -38,12 +42,38 @@ public class ClienteController {
 		return "form";
 	}
 	@RequestMapping(value = "/form", method = RequestMethod.POST)
-	public String guardar(@Valid Cliente cliente, BindingResult result) {
+	public String guardar(@Valid Cliente cliente, BindingResult result, Model model, SessionStatus status) {
 		if(result.hasErrors()) {
+			model.addAttribute("titulo", "Formulario de cliente");
 			return "form";
 		}
 		clienteDao.save(cliente);
+		status.setComplete();
 		return "redirect:listar";
+	}
+	@RequestMapping(value="/form/{id}")
+	public String editar(@PathVariable(value = "id") Long id,Map<String, Object> model) {
+		
+		Cliente cliente = null;
+		
+			
+		if(id > 0) {
+			cliente = clienteDao.findOne(id);	
+			}else {
+				return "redirect:/listar";
+			}
+		model.put("cliente", cliente);
+		model.put("titulo", "Editar cliente");
+		
+		return "form";
+	}
+	@RequestMapping(value="/eiminar/{id}")
+	public String eliminar(@PathVariable(value = "id") Long id) {
+		if(id > 0) {
+			  clienteDao.delete(id);
+		}
+		
+		return "redirect;/listar";
 	}
 	
 }
--- src/main/java/com/bolsadeideas/springboot/app/models/service/IClienteService.java
+++ src/main/java/com/bolsadeideas/springboot/app/models/service/IClienteService.java
@@ -1,13 +1,12 @@
-package com.bolsadeideas.springboot.app.models.dao;
+package com.bolsadeideas.springboot.app.models.service;
 
 import java.util.List;
 
 import com.bolsadeideas.springboot.app.models.entity.Cliente;
 
-public interface IClienteDao {
+public interface IClienteService {
 	public List<Cliente> findAll();
 	public void save(Cliente cliente);
 	public Cliente findOne(Long id);
 	public void delete(Long id);
-	
 }
